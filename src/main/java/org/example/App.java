50dd4a908e86c46abdf042edfc4e5b8fc7592706
Oleg Hovsepyan <oleghovsepyan@cloud.upwork.com>
2022-11-01 02:22:55 -0700

    Task: Make algorithm complexity n

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,10 +10,36 @@ public class App
             return 0;
         }
         long[] result = new long[landscape.length];
+        long[] highestLeftPeaks = calculateHighestLeftPeaks(landscape);
+        long[] highestRightPeaks = calculateHighestRightPeaks(landscape);
         for(int i = 0; i < landscape.length; i++) {
-            result[i] = calculateWaterAmountForIndex(i, landscape);
+            result[i] = calculateWaterAmountForIndex(i, landscape, highestLeftPeaks, highestRightPeaks);
         }
         return LongStream.of(result).sum();
+    }
+
+    public static long[] calculateHighestRightPeaks(int[] landscape) {
+        long[] result = new long[landscape.length];
+        long currentHighestPeak = 0;
+        for(int i = landscape.length - 2; i >= 0; i--) {
+            if(landscape[i + 1] > currentHighestPeak) {
+                currentHighestPeak = landscape[i + 1];
+            }
+            result[i] = currentHighestPeak;
+        }
+        return result;
+    }
+
+    public static long[] calculateHighestLeftPeaks(int[] landscape) {
+        long[] result = new long[landscape.length];
+        long currentHighestPeak = 0;
+        for(int i = 1; i < landscape.length; i++) {
+            if(landscape[i - 1] > currentHighestPeak) {
+                currentHighestPeak = landscape[i - 1];
+            }
+            result[i] = currentHighestPeak;
+        }
+        return result;
     }
 
     public static void checkArgument(int[] landscape) {
@@ -27,13 +53,13 @@ public class App
         }
     }
 
-    public static long calculateWaterAmountForIndex(int index, int[] landscape) {
+    public static long calculateWaterAmountForIndex(int index, int[] landscape, long[] highestLeftPeaks, long[] highestRightPeaks) {
         if(index == 0 || index == landscape.length - 1) {
             return 0;
         } else {
             long currentHeight = landscape[index];
-            long highestLeftPeak = findHighestLeftPeak(index, landscape);
-            long highestRightPeak = findHighestRightPeak(index, landscape);
+            long highestLeftPeak = highestLeftPeaks[index];
+            long highestRightPeak = highestRightPeaks[index];
             if(highestLeftPeak > currentHeight && highestRightPeak > currentHeight) {
                 long smallestPeak = Math.min(highestLeftPeak, highestRightPeak);
                 return smallestPeak - currentHeight;
@@ -41,25 +67,5 @@ public class App
                 return 0;
             }
         }
-    }
-
-    public static long findHighestLeftPeak(int index, int[] landscape) {
-        int highest = -1;
-        for(int i = index - 1; i >= 0; i--) {
-            if(landscape[i] > highest) {
-                highest = landscape[i];
-            }
-        }
-        return highest;
-    }
-
-    public static long findHighestRightPeak(int index, int[] landscape) {
-        int highest = -1;
-        for(int i = index + 1; i < landscape.length; i++) {
-            if(landscape[i] > highest) {
-                highest = landscape[i];
-            }
-        }
-        return highest;
     }
 }
